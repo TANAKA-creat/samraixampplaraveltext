221bd911227835f000bf37097950261bfb0356cb
tanaka <tanakakenji@126.com>
2022-10-13 08:50:04 +0800

    初回コミット

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+<nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
+    <div class="container">
+        <a href="{{ route('posts.index') }}" class="nabver-brand">投稿アプリ</a>
+    </div>
+</nav>
+</header>
+
